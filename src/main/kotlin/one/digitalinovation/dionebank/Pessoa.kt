1481a5bfa986014b0a6e72aab1f3553c982926fc
Miguel Pereira <miguelpereira@MacBook-de-Miguel.local>
2021-04-27 22:59:09 -0300

    updated to apply the private modifier to ser function of cpf proprertd

--- src/main/kotlin/one/digitalinovation/dionebank/Pessoa.kt
+++ src/main/kotlin/one/digitalinovation/dionebank/Pessoa.kt
@@ -1,13 +1,15 @@
 package one.digitalinovation.dionebank
 
-class Pessoa {
+ class  Pessoa{
     var nome: String = "miguel"
     var cpf: String = "419.321.608-28"
+    private  set
 
 }
 
 fun main() {
     val miguel = Pessoa()
+    println(miguel)
     println(miguel.nome)
     println(miguel.cpf)
 }
